89ccabd3a95e0639d4536df0881d82c4ac022eb8
beatyman <1256028890@qq.com>
2021-09-23 09:50:00 +0800

    test

--- cmd/server.go
+++ cmd/server.go
@@ -34,7 +34,7 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("server called")
-		if err := server.ServeRPC(cmd.Context(), ":3000", 1024*1024); err != nil {
+		if err := server.ServeRPC(cmd.Context(), ":3500", 1024*1024); err != nil {
 			log.Error(err)
 		}
 	},
